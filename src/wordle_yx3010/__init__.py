f6e7594150defe81ca7017e2fa24d6309bccdcf6
Nicole <yx3010@columbia.edu>
2025-10-23 12:01:50 -0400

    b- function & example

--- src/wordle_yx3010/__init__.py
+++ src/wordle_yx3010/__init__.py
@@ -1,3 +1,19 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("wordle_yx3010")
+try:
+    from importlib.metadata import version
+    __version__ = version("wordle_yx3010")
+except Exception:
+    __version__ = "0.1.0"
+
+from .wordle_yx3010 import (
+    validate_guess,
+    check_guess,
+    is_valid_word,
+    calculate_game_score,
+)
+
+__all__ = [
+    "validate_guess",
+    "check_guess",
+    "is_valid_word",
+    "calculate_game_score",
+]
