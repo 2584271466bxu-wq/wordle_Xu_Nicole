267fe83f0ae9d0f7c462319267f8a34bf2e143d5
Nicole <yx3010@columbia.edu>
2025-10-23 12:07:19 -0400

    c - documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys
+sys.path.insert(0, os.path.abspath("../src"))
 
-# -- Project information -----------------------------------------------------
+project = "wordle_yx3010"
+author = "Nicole"
+release = "0.1.0"
 
-project = u"wordle_yx3010"
-copyright = u"2025, nicole"
-author = u"nicole"
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "myst_nb",
-    "autoapi.extension",
+    "myst_parser",
+    "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
 ]
-autoapi_dirs = ["../src"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+wordle_yx3010 documentation
+===========================
+
+API Reference
+=============
+
+.. automodule:: wordle_yx3010.wordle_yx3010
+   :members:
+   :undoc-members:
+   :show-inheritance:
